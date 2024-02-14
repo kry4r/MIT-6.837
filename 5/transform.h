e976736fbc14114f4d4bb527cd4c20da24427d00
Thibault Zhang <a709635763@outlook.com>
2024-02-14 15:43:11 +0800

    A5 done

---
+++ 5/transform.h
@@ -0,0 +1,26 @@
+//
+// Created by Nidhogg on 2024/2/7.
+//
+
+#ifndef ASSIGNMENT_TRANSFORM_H
+#define ASSIGNMENT_TRANSFORM_H
+
+#include "object3d.h"
+#include "matrix.h"
+
+class Transform : public Object3D {
+    Matrix _mat, _invMat, _invTpMat;
+    Object3D *_object;
+
+public:
+    Transform(const Matrix &mat, Object3D *object);
+
+    bool intersect(const Ray &r, Hit &h, float tmin) override;
+    void paint() override;
+
+    void insertIntoGrid(Grid *g, Matrix *m) override;
+
+    static BoundingBox TransformBoundingBox(const Object3D &object, const Matrix &m);
+};
+
+#endif //ASSIGNMENTS_TRANSFORM_H
